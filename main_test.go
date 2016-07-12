a9eaa42bc8f74e5eb24733c797b6652be57926f5
Mariano Gappa <mariano@movio.co>
2016-07-12 21:50:45 +1200

    Tests ReadCmd.

--- main_test.go
+++ main_test.go
@@ -24,3 +24,20 @@ two`
 		t.Error("second line wasn't 'two'")
 	}
 }
+
+func TestReadCmd(t *testing.T) {
+	cmdString := `echo -e "one\ntwo"`
+	o := make(chan string, 2)
+
+	readCmd(cmdString, o)
+
+	line1 := <-o
+	line2 := <-o
+
+	if line1 != "one" {
+		t.Error("first line wasn't 'one'")
+	}
+	if line2 != "two" {
+		t.Error("second line wasn't 'two'")
+	}
+}

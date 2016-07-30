17f7ce9ec06d1522f6e031679c71c8131d5067f9
Mariano Gappa <mariano@movio.co>
2016-07-30 13:21:16 +1200

    Tests resolving timeouts.

--- args_test.go
+++ args_test.go
@@ -3,112 +3,123 @@ package main
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
-var tests = []struct {
-	args     []string
-	expected options
-	fails    bool
-}{
-	{
-		args: []string{},
-		expected: options{
-			follow:      false,
-			infinite:    false,
-			patience:    -1,
-			timeoutF:    10,
-			hardTimeout: 0,
-		},
-	},
-	{
-		args: []string{"-f"},
-		expected: options{
-			follow:      true,
-			infinite:    false,
-			patience:    -1,
-			timeoutF:    10,
-			hardTimeout: 0,
-		},
-	},
-	{
-		args: []string{"-i"},
-		expected: options{
-			follow:      false,
-			infinite:    true,
-			patience:    -1,
-			timeoutF:    10,
-			hardTimeout: 0,
-		},
-	},
-	{
-		args: []string{"-p", "0"},
-		expected: options{
-			follow:      false,
-			infinite:    false,
-			patience:    0,
-			timeoutF:    10,
-			hardTimeout: 0,
-		},
-	},
-	{
-		args: []string{"-t", "5"},
-		expected: options{
-			follow:      false,
-			infinite:    false,
-			patience:    -1,
-			timeoutF:    5,
-			hardTimeout: 0,
-		},
-	},
-	{
-		args: []string{"-h", "120"},
-		expected: options{
-			follow:      false,
-			infinite:    false,
-			patience:    -1,
-			timeoutF:    10,
-			hardTimeout: 120,
-		},
-	},
-	{
-		args: []string{"-i", "-f"},
-		expected: options{
-			follow:      true,
-			infinite:    true,
-			patience:    -1,
-			timeoutF:    10,
-			hardTimeout: 0,
-		},
-	},
-	{
-		args:  []string{"-p"},
-		fails: true,
-	},
-	{
-		args:  []string{"-t"},
-		fails: true,
-	},
-	{
-		args:  []string{"-h"},
-		fails: true,
-	},
-	{
-		args:  []string{"-if"},
-		fails: true,
-	},
-	{
-		args: []string{"-f", "-t", "1", "-i", "-p", "2", "-h", "3"},
-		expected: options{
-			follow:      true,
-			infinite:    true,
-			patience:    2,
-			timeoutF:    1,
-			hardTimeout: 3,
-		},
-	},
-}
-
 func TestResolveOptions(t *testing.T) {
+	tests := []struct {
+		args     []string
+		expected options
+		fails    bool
+	}{
+		{
+			args: []string{},
+			expected: options{
+				follow:      false,
+				infinite:    false,
+				patience:    -1,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+		},
+		{
+			args: []string{"-f"},
+			expected: options{
+				follow:      true,
+				infinite:    false,
+				patience:    -1,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+		},
+		{
+			args: []string{"-i"},
+			expected: options{
+				follow:      false,
+				infinite:    true,
+				patience:    -1,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+		},
+		{
+			args: []string{"-p", "0"},
+			expected: options{
+				follow:      false,
+				infinite:    false,
+				patience:    0,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+		},
+		{
+			args: []string{"-t", "5"},
+			expected: options{
+				follow:      false,
+				infinite:    false,
+				patience:    -1,
+				timeoutF:    5,
+				hardTimeout: 0,
+			},
+		},
+		{
+			args: []string{"-h", "120"},
+			expected: options{
+				follow:      false,
+				infinite:    false,
+				patience:    -1,
+				timeoutF:    10,
+				hardTimeout: 120,
+			},
+		},
+		{
+			args: []string{"-i", "-f"},
+			expected: options{
+				follow:      true,
+				infinite:    true,
+				patience:    -1,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+		},
+		{
+			args:  []string{"-p"},
+			fails: true,
+		},
+		{
+			args:  []string{"-t"},
+			fails: true,
+		},
+		{
+			args:  []string{"-h"},
+			fails: true,
+		},
+		{
+			args:  []string{"-if"},
+			fails: true,
+		},
+		{
+			args: []string{"-f", "-t", "1", "-i", "-p", "2", "-h", "3"},
+			expected: options{
+				follow:      true,
+				infinite:    true,
+				patience:    2,
+				timeoutF:    1,
+				hardTimeout: 3,
+			},
+		},
+		{
+			args: []string{"--follow", "--timeout", "1", "--infinite", "--patience", "2", "--hard-timeout", "3"},
+			expected: options{
+				follow:      true,
+				infinite:    true,
+				patience:    2,
+				timeoutF:    1,
+				hardTimeout: 3,
+			},
+		},
+	}
+
 	for _, ts := range tests {
 		result, err := resolveOptions(ts.args)
 
@@ -122,6 +133,188 @@ func TestResolveOptions(t *testing.T) {
 
 		if !ts.fails && !reflect.DeepEqual(*result, ts.expected) {
 			t.Errorf("default options are incorrect: %v was not equal to %v", result, ts.expected)
+		}
+	}
+}
+
+func TestResolveTimeouts(t *testing.T) {
+	tests := []struct {
+		options      *options
+		stdinTimeout timeout
+		cmdTimeout   timeout
+	}{
+		{
+			options: &options{
+				follow:      false,
+				infinite:    false,
+				patience:    -1,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+			stdinTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          false,
+				firstTime:         10 * time.Second,
+				time:              10 * time.Second,
+			},
+			cmdTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          false,
+				firstTime:         10 * time.Second,
+				time:              10 * time.Second,
+			},
+		},
+		{
+			options: &options{
+				follow:      true,
+				infinite:    false,
+				patience:    -1,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+			stdinTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          true,
+				firstTime:         10 * time.Second,
+				time:              10 * time.Second,
+			},
+			cmdTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          false,
+				firstTime:         10 * time.Second,
+				time:              10 * time.Second,
+			},
+		},
+		{
+			options: &options{
+				follow:      false,
+				infinite:    true,
+				patience:    -1,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+			stdinTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          false,
+				firstTime:         10 * time.Second,
+				time:              10 * time.Second,
+			},
+			cmdTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          true,
+				firstTime:         10 * time.Second,
+				time:              10 * time.Second,
+			},
+		},
+		{
+			options: &options{
+				follow:      false,
+				infinite:    false,
+				patience:    0,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+			stdinTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: true,
+				infinite:          false,
+				firstTime:         0 * time.Second,
+				time:              10 * time.Second,
+			},
+			cmdTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: true,
+				infinite:          false,
+				firstTime:         0 * time.Second,
+				time:              10 * time.Second,
+			},
+		},
+		{
+			options: &options{
+				follow:      true,
+				infinite:    false,
+				patience:    20,
+				timeoutF:    10,
+				hardTimeout: 0,
+			},
+			stdinTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          true,
+				firstTime:         20 * time.Second,
+				time:              10 * time.Second,
+			},
+			cmdTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          false,
+				firstTime:         20 * time.Second,
+				time:              10 * time.Second,
+			},
+		},
+		{
+			options: &options{
+				follow:      false,
+				infinite:    false,
+				patience:    -1,
+				timeoutF:    10,
+				hardTimeout: 120,
+			},
+			stdinTimeout: timeout{
+				hard:              true,
+				firstTimeInfinite: false,
+				infinite:          false,
+				firstTime:         120 * time.Second,
+				time:              10 * time.Second,
+			},
+			cmdTimeout: timeout{
+				hard:              true,
+				firstTimeInfinite: false,
+				infinite:          false,
+				firstTime:         120 * time.Second,
+				time:              10 * time.Second,
+			},
+		},
+		{
+			options: &options{
+				follow:      true,
+				infinite:    false,
+				patience:    -1,
+				timeoutF:    30,
+				hardTimeout: 0,
+			},
+			stdinTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          true,
+				firstTime:         30 * time.Second,
+				time:              30 * time.Second,
+			},
+			cmdTimeout: timeout{
+				hard:              false,
+				firstTimeInfinite: false,
+				infinite:          false,
+				firstTime:         30 * time.Second,
+				time:              30 * time.Second,
+			},
+		},
+	}
+
+	for _, ts := range tests {
+		stdinTimeout, cmdTimeout := resolveTimeouts(ts.options)
+
+		if !reflect.DeepEqual(stdinTimeout, ts.stdinTimeout) {
+			t.Errorf("stdinTimeout resolved incorrectly: %v was not equal to %v", stdinTimeout, ts.stdinTimeout)
+		}
+
+		if !reflect.DeepEqual(cmdTimeout, ts.cmdTimeout) {
+			t.Errorf("cmdTimeout resolved incorrectly: %v was not equal to %v", cmdTimeout, ts.cmdTimeout)
 		}
 	}
 }
